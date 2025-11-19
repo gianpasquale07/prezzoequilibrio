ed6f32c39948b4e6de25f44b1b79e39699e434cd
piromalli.21229 <piromalli.21229@LABI1-13PC23>
2025-11-19 13:29:03 +0100

    Aggiungere i file di progetto.

---
+++ es1_19_11/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace es1_19_11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_calcola_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txb_q.Text)) throw new Exception("errore");
+
+            int q;
+            if (int.TryParse(txb_q.Text, out q) == false) throw new Exception("errore");
+
+            int d = 90 - (4 * q);
+            int o = 10 + (q ^ 3 / 100);
+
+            MessageBox.Show($"la domanda è : d = {d} e l'offerta è : o = {o}");
+        }
+    }
+}
